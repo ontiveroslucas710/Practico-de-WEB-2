a32b7f47d03a2cbbd5027bc32d45aca278a18ae6
lucas ontiveros <ontiveroslucas710@gmail.com>
2020-12-02 21:33:54 -0300

    se agrego base de datos

--- db_ropa.sql
+++ db_ropa.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-11-2020 a las 15:17:33
--- Versión del servidor: 10.4.13-MariaDB
--- Versión de PHP: 7.4.7
+-- Tiempo de generación: 03-12-2020 a las 01:32:51
+-- Versión del servidor: 10.4.14-MariaDB
+-- Versión de PHP: 7.4.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -52,7 +52,24 @@ INSERT INTO `comentarios` (`id`, `comentarios`, `puntaje`, `commentedBy`, `id_co
 (148, 'prueba', 2, 'unodeprueba', 30),
 (149, 'ahora ? ', 1, 'admin unodeprueba', 30),
 (150, 'ajskldas', 1, 'admin mariano123', 24),
-(151, 'masdas', 1, 'admin mariano123', 24);
+(151, 'masdas', 1, 'admin mariano123', 24),
+(152, 'nuevo comentario de lucas', 2, 'admin lucas123', 24),
+(153, 'comentario de puntaje 4', 4, 'admin lucas123', 24),
+(154, 'comentario de puntaje 4', 4, 'admin lucas123', 24),
+(155, 'comentario de puntaje 4', 4, 'admin lucas123', 24),
+(156, 'mi primer comentario', 1, 'admin lucas123', 54),
+(157, 'mi segundo comentario', 2, 'admin lucas123', 54),
+(158, 'solo comentario', 2, 'usuario1', 31),
+(159, 'comentarios', 1, 'admin lucas123', 22),
+(161, 'sfdsf', 3, 'admin lucas123', 22),
+(166, 'sfdsf', 4, 'admin lucas123', 22),
+(167, 'otor coment', 2, 'usuario83', 22),
+(168, 'otor coment', 2, 'usuario83', 22),
+(169, 'muy bueno el pantalon', 4, 'admin lucas123', 50),
+(170, 'es durable, recomndado', 5, 'usuario1', 52),
+(171, 'comentario con buena calificacion', 3, 'usuario1', 22),
+(172, 'muy buena coleccion', 3, 'usuario', 94),
+(173, 'calidad buena', 4, 'usuario', 94);
 
 -- --------------------------------------------------------
 
@@ -79,7 +96,8 @@ INSERT INTO `marca` (`id_marca`, `descripcion`, `marca`) VALUES
 (12, 'descripcion de bearcliff', 'BEARCLIFF'),
 (13, 'descripcion de basement', 'BASEMENT'),
 (14, 'descripcion de americanino', 'AMERICANINO'),
-(15, 'descripcion de mossimo', 'MOSSIMO');
+(15, 'descripcion de mossimo', 'MOSSIMO'),
+(28, 'PARA IR CERQUITA', 'CERCANI');
 
 -- --------------------------------------------------------
 
@@ -103,12 +121,17 @@ CREATE TABLE `pantalon` (
 --
 
 INSERT INTO `pantalon` (`id_pantalones`, `nombre`, `talle`, `color`, `tela`, `precio`, `imagen`, `id_marca`) VALUES
-(17, 'sarandunga', 40, 'azul claro', 'algodon', 3490, 'pantalon-muestra.jpg', 3),
-(22, 'Rally', 35, 'azul oscuro', 'estilizada', 2990, NULL, 7),
-(24, 'cosa nueva', 43, 'celeste', 'mezclilla', 2900, NULL, 3),
-(30, 'pantalón chino con cinturón', 43, 'crema', 'algodon', 5900, NULL, 13),
-(31, 'balcarce', 38, 'beige', 'algodón ', 2300, NULL, 6),
-(50, 'mariano', 456, 'sda', 'das', 456, 'C5B2.tmp', 3);
+(22, 'Rally', 35, 'azul oscuro', 'estilizada', 2990, 'pantalon-muestra.jpg', 28),
+(50, 'mariano', 456, 'sda', 'das', 456, NULL, 11),
+(52, 'otro lucas', 40, 'gris', 'suave', 6088, 'parabestir.jpg', 3),
+(66, 'pantalon chupin', 42, 'marron claro', 'elastizada', 3570, 'panta_rojo.jpg', 7),
+(68, 'panta vestir', 42, 'azul marino', '40', 3000, 'gabardina.jpg', 12),
+(85, 'veige', 34, 'mostaza', 'ajustable', 3450, 'panta_mostaza.jpg', 13),
+(88, 'casual', 39, 'blanco', 'suave', 2570, 'parabestir.jpg', 15),
+(89, 'prima-verano', 42, 'celeste', 'elastizada', 4380, 'casual_azu.jpg', 3),
+(92, 'lucas', 4, '3', '5', 6, 'Koala.jpg', 3),
+(93, 'vestiment', 45, 'gris', 'tipo traje', 3870, 'php4EA4.tmp', 10),
+(94, 'clasic', 42, 'azul', 'vaquero', 25000, 'muchos_panta.jpg', 6);
 
 -- --------------------------------------------------------
 
@@ -130,11 +153,10 @@ CREATE TABLE `usuario` (
 INSERT INTO `usuario` (`id`, `nombre`, `password`, `administrador`) VALUES
 (5, 'lucas123', '$2y$10$oTxg6SodRUUZRpcNnOYgaua.tYZ66MlJV.TFzcB33LC9gYEPWR3Ya', 1),
 (8, 'mariano123', '$2y$10$nQTJWkxZfULLNkmBK.pOvOk4pKBiM2Tcaqx2i/9xEf/pjDrArJk0K', 1),
-(9, 'usuario1', '$2y$10$tvd32pOWVMlQ1rEnwk4jHeURIDcpSMep4BEZ/U3eqike8EIkU/6fO', 1),
-(44, 'usuario2', '$2y$10$bkDIvef/grr/DcsGgwBCWeN4/OfB.a3cIzeq5yvXF88Hsrq7ppgwa', 0),
 (45, 'marianomayo', '$2y$10$/wudlfVYF0Yl57.DPnEK2eQPNC0f5q6RFQsJqf.YQxxnIL6MGpjYy', 0),
-(46, 'camila', '$2y$10$AdTaxh3ihi1YqcLMeQrnouMWR47Ym0tAzWaBUGwQAu7poKWejKDKS', 0),
-(47, 'unodeprueba', '$2y$10$zbUT9gHiFFacylQEeFCQ6.s3hXwlhG/UYdPa/0ANpAmVcCFKy31yq', 1);
+(47, 'unodeprueba', '$2y$10$zbUT9gHiFFacylQEeFCQ6.s3hXwlhG/UYdPa/0ANpAmVcCFKy31yq', 0),
+(49, 'usuario83', '$2y$10$WLHtrdO5rCODAlxidb9y2.fs97N.DJZmvQqyu1vC43u5H5jlGdzp2', 0),
+(51, 'usuario', '$2y$10$NdV1/BEfU4Dsj5WMs9VriOZ2JatZeLwCGjNAr0VHB8pTCDSlXW3b2', 0);
 
 --
 -- Índices para tablas volcadas
@@ -174,25 +196,25 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `comentarios`
 --
 ALTER TABLE `comentarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=152;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=174;
 
 --
 -- AUTO_INCREMENT de la tabla `marca`
 --
 ALTER TABLE `marca`
-  MODIFY `id_marca` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id_marca` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT de la tabla `pantalon`
 --
 ALTER TABLE `pantalon`
-  MODIFY `id_pantalones` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+  MODIFY `id_pantalones` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=95;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
 
 --
 -- Restricciones para tablas volcadas
